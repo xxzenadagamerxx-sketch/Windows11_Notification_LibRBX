7571becebc81c5bb91c6f29436775eb49053e630
xxzenadagamerxx-sketch <xxzenadagamerxx@gmail.com>
2026-01-14 11:11:23 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -3,5 +3,12 @@ Library.MakeWindow("Example","This is an example window.",false)
 --[[ Arguments: 
 Arg 1 = WindowTitle (string)
 Arg 2 = MainText (string)
-Arg 3 = Debug (bool)
+Arg 3 = Debug (boolean,Optional)
+Arg 4 = Sound (boolean,Optional)
 --]]
+Library.OK.MouseButton1Click:Connect(function()
+print("Pressed OK")
+  end)
+Library.Cancel.MouseButton1Click:Connect(function()
+print("Pressed Cancel")
+  end)
