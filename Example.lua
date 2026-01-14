8f2df5d12383e685f45080fd6502bd3b4c817922
xxzenadagamerxx-sketch <xxzenadagamerxx@gmail.com>
2026-01-14 10:31:21 +0200

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,6 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xxzenadagamerxx-sketch/Windows11_Notification_LibRBX/refs/heads/main/Source.lua"))()
+Library.MakeWindow("Example","This is an example window.",false) 
+--[[ Arguments: 
+Arg 1 = WindowTitle (string)
+Arg 2 = MainText (string)
+Arg 3 = Debug (bool)
