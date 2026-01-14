cd77b564a407ad041eb4016ede5f386fecea9b9d
xxzenadagamerxx-sketch <xxzenadagamerxx@gmail.com>
2026-01-14 11:31:30 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,14 +1,14 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xxzenadagamerxx-sketch/Windows11_Notification_LibRBX/refs/heads/main/Source.lua"))()
-Library.MakeWindow("Example","This is an example window.",false) 
+local Window = Library.MakeWindow("Example","This is an example window.",false,true) 
 --[[ Arguments: 
 Arg 1 = WindowTitle (string)
 Arg 2 = MainText (string)
 Arg 3 = Debug (boolean,Optional)
 Arg 4 = Sound (boolean,Optional)
 --]]
-Library.OK.MouseButton1Click:Connect(function()
+Window.OK.MouseButton1Click:Connect(function()
 print("Pressed OK")
   end)
-Library.Cancel.MouseButton1Click:Connect(function()
+Window.Cancel.MouseButton1Click:Connect(function()
 print("Pressed Cancel")
   end)
